5b90b01852c1e471e2c96b48dbc47d1110c3aeab
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-01-12 15:42:36 +0100

    docker has been initialised using all allowerd host

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    picture = models.TextField(blank=False, null=False)
+    active = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.title
