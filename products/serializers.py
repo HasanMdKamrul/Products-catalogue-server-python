f5c44debcb9eba7dbee444c1475bfdae6f1c830b
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-01-12 16:52:37 +0100

    customise temp_price according to time added

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,18 @@
+import datetime
+import locale
+
+import pytz
 from rest_framework import serializers
 
 from .models import Product
 
+# ** Datetime import 
+
+
+# ** Standard Berlin time get
+time_zone = pytz.timezone('Europe/Berlin')
+berlin_time = datetime.datetime.now(time_zone)
+berlin_current_time = berlin_time.strftime("%H:%M:%S")
 
 class ProductSerializer(serializers.ModelSerializer):
     temp_price = serializers.SerializerMethodField()
@@ -17,5 +28,8 @@ class ProductSerializer(serializers.ModelSerializer):
             "temp_price"
         )
         
-    def get_temp_price(self, obj):    
-        return obj.price * 1.2
+    def get_temp_price(self, obj):
+        if berlin_current_time > "11:00:00" and berlin_current_time < "15:00:00":
+            return  obj.price * 0.1
+        else:
+            return obj.price
